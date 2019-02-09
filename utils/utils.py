84a42c2bc1163c471d440bf96ad63bbcf80e288a
evanfeng <evanfeng@pku.edu.cn>
2019-02-09 17:43:51 +0000

    add freezing/unfreezing utilities

--- utils/utils.py
+++ utils/utils.py
@@ -1,2 +1,12 @@
 def print_ids(ids, vocab):
     print(' '.join(vocab.idx2w[i] for i in ids))
+
+
+def freeze_net(net):
+    for p in net.parameters():
+        p.requires_grad = False
+
+
+def unfreeze_net(net):
+    for p in net.parameters():
+        p.requires_grad = True
