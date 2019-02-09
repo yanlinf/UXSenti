50bc2745ac19ee26c1176f0450f12e7b12f58768
evanfeng <evanfeng@pku.edu.cn>
2019-02-09 17:46:52 +0000

    refactor, add use_wgan arg

--- trainer.py
+++ trainer.py
@@ -8,7 +8,9 @@ from utils.bdi import compute_nn_accuracy
 
 class CrossLingualLanguageModelTrainer(object):
 
-    def __init__(self, src_lm, trg_lm, discriminator, lm_optimizer, dis_optimizer, criterion, bptt, alpha, beta, lambd, lm_clip, dis_clip, lexicon, lexicon_size):
+    def __init__(self, src_lm, trg_lm, discriminator, lm_optimizer, dis_optimizer,
+                 criterion, bptt, alpha, beta, lambd, lm_clip, dis_clip, lexicon,
+                 lexicon_size, use_wgan=False):
         self.src_lm = src_lm
         self.trg_lm = trg_lm
         self.discriminator = discriminator
@@ -23,8 +25,38 @@ class CrossLingualLanguageModelTrainer(object):
         self.dis_clip = dis_clip
         self.lexicon = lexicon
         self.lexicon_size = lexicon_size
+        self.use_wgan = use_wgan
         self.rev_grad = GradReverse(self.lambd)
         self.is_cuda = next(self.src_lm.parameters()).is_cuda
+
+    def compute_loss(self, src_x, src_y, trg_x, trg_y):
+        bs, bptt = src_x.size()
+        src_out, src_h, src_dropped_h = self.src_lm(src_x)
+        trg_out, trg_h, trg_dropped_h = self.trg_lm(trg_x)
+        src_loss = src_raw_loss = self.criterion(F.log_softmax(src_out.view(-1, src_out.size(-1)), -1), src_y)
+        trg_loss = trg_raw_loss = self.criterion(F.log_softmax(trg_out.view(-1, trg_out.size(-1)), -1), trg_y)
+
+        src_loss = src_loss + sum(self.alpha * h.pow(2).mean() for h in src_dropped_h[-1:])
+        src_loss = src_loss + sum(self.beta * (h[:, 1:] - h[:, :-1]).pow(2).mean() for h in src_h[-1:])
+        trg_loss = trg_loss + sum(self.alpha * h.pow(2).mean() for h in trg_dropped_h[-1:])
+        trg_loss = trg_loss + sum(self.beta * (h[:, 1:] - h[:, :-1]).pow(2).mean() for h in trg_h[-1:])
+
+        src_pooled = torch.cat([h.mean(1) for h in src_h], -1)
+        trg_pooled = torch.cat([h.mean(1) for h in trg_h], -1)
+        dis_x = torch.cat([src_pooled, trg_pooled], 0)
+        dix_x = self.rev_grad(dis_x)
+        dis_y = torch.cat((torch.zeros(bs, dtype=torch.int64), torch.ones(bs, dtype=torch.int64)), -1)
+        if self.is_cuda:
+            dis_y = dis_y.cuda()
+
+        dis_out = self.discriminator(dis_x)
+
+        if self.use_wgan:
+            dis_loss = dis_out[:bs].mean() - dis_out[bs:].mean()
+        else:
+            dis_loss = self.criterion(F.log_softmax(dis_out, -1), dis_y)
+
+        return src_raw_loss, trg_raw_loss, src_loss, trg_loss, dis_loss
 
     def step(self, src_x, src_y, trg_x, trg_y):
         """
@@ -38,41 +70,23 @@ class CrossLingualLanguageModelTrainer(object):
 
         self.lm_optimizer.zero_grad()
         self.dis_optimizer.zero_grad()
-
         lr0 = self.lm_optimizer.param_groups[0]['lr']
         self.lm_optimizer.param_groups[0]['lr'] = lr0 * bptt / self.bptt
 
-        src_out, src_h, src_dropped_h = self.src_lm(src_x)
-        trg_out, trg_h, trg_dropped_h = self.trg_lm(trg_x)
-        src_loss = src_raw_loss = self.criterion(F.log_softmax(src_out.view(-1, src_out.size(-1)), -1), src_y)
-        trg_loss = trg_raw_loss = self.criterion(F.log_softmax(trg_out.view(-1, trg_out.size(-1)), -1), trg_y)
-        src_loss = src_loss + sum(self.alpha * h.pow(2).mean() for h in src_dropped_h[-1:])
-        src_loss = src_loss + sum(self.beta * (h[:, 1:] - h[:, :-1]).pow(2).mean() for h in src_h[-1:])
-        trg_loss = trg_loss + sum(self.alpha * h.pow(2).mean() for h in trg_dropped_h[-1:])
-        trg_loss = trg_loss + sum(self.beta * (h[:, 1:] - h[:, :-1]).pow(2).mean() for h in trg_h[-1:])
-
-        src_pooled = torch.cat([h.mean(1) for h in src_h], -1)
-        trg_pooled = torch.cat([h.mean(1) for h in trg_h], -1)
-        dis_x = torch.cat([src_pooled, trg_pooled], 0)
-        dix_x = self.rev_grad(dis_x)
-        dis_y = torch.cat((torch.zeros(bs, dtype=torch.int64), torch.ones(bs, dtype=torch.int64)), -1)
-        if self.is_cuda:
-            dis_y = dis_y.cuda()
-        dis_out = self.discriminator(dis_x)
-        dis_loss = self.criterion(F.log_softmax(dis_out, -1), dis_y)
-
+        src_raw_loss, trg_raw_loss, src_loss, trg_loss, dis_loss = self.compute_loss(src_x, src_y, trg_x, trg_y)
         loss = src_loss + trg_loss + dis_loss
-        loss.backward()
+
+        if self.use_wgan:
+            (src_loss + trg_loss - dis_loss).backward()
+        else:
+            loss.backward()
 
         torch.nn.utils.clip_grad_norm_(self.src_lm.parameters(), self.lm_clip)
         torch.nn.utils.clip_grad_norm_(self.trg_lm.parameters(), self.lm_clip)
-
         self.lm_optimizer.step()
         self.dis_optimizer.step()
-
         for x in self.discriminator.parameters():
             x.data.clamp_(-self.dis_clip, self.dis_clip)
-
         self.lm_optimizer.param_groups[0]['lr'] = lr0
 
         return loss, src_raw_loss, trg_raw_loss, dis_loss
@@ -92,34 +106,13 @@ class CrossLingualLanguageModelTrainer(object):
         self.reset()
 
         total_losses = np.zeros(4)
-
         for i in range(0, length, bptt):
             sx = src_val[i:i + bptt].t()
             sy = src_val[i + 1:i + 1 + bptt].t().contiguous().view(-1)
             tx = trg_val[i:i + bptt].t()
             ty = trg_val[i + 1:i + 1 + bptt].t().contiguous().view(-1)
-            src_out, src_h, src_dropped_h = self.src_lm(sx)
-            trg_out, trg_h, trg_dropped_h = self.trg_lm(tx)
-            src_loss = src_raw_loss = self.criterion(F.log_softmax(src_out.view(-1, src_out.size(-1)), -1), sy)
-            trg_loss = trg_raw_loss = self.criterion(F.log_softmax(trg_out.view(-1, trg_out.size(-1)), -1), ty)
-            src_loss = src_loss + sum(self.alpha * h.pow(2).mean() for h in src_dropped_h[-1:])
-            src_loss = src_loss + sum(self.beta * (h[:, 1:] - h[:, :-1]).pow(2).mean() for h in src_h[-1:])
-            trg_loss = trg_loss + sum(self.alpha * h.pow(2).mean() for h in trg_dropped_h[-1:])
-            trg_loss = trg_loss + sum(self.beta * (h[:, 1:] - h[:, :-1]).pow(2).mean() for h in trg_h[-1:])
-
-            src_pooled = torch.cat([h.mean(1) for h in src_h], -1)
-            trg_pooled = torch.cat([h.mean(1) for h in trg_h], -1)
-
-            dx = torch.cat([src_pooled, trg_pooled], 0)
-            dx = self.rev_grad(dx)
-            dy = torch.cat((torch.zeros(bs, dtype=torch.int64), torch.ones(bs, dtype=torch.int64)), -1)
-            if self.is_cuda:
-                dy = dy.cuda()
-
-            dis_out = self.discriminator(dx)
-            dis_loss = self.criterion(F.log_softmax(dis_out, -1), dy)
+            src_raw_loss, trg_raw_loss, src_loss, trg_loss, dis_loss = self.compute_loss(sx, sy, tx, ty)
             loss = src_loss + trg_loss + dis_loss
-
             total_losses += np.array([loss, src_raw_loss, trg_raw_loss, dis_loss])
 
         return total_losses / (length / bptt)
