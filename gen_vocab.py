ba9f3a0ebce2d0c6be2d5ff0356db488ea0a7435
evanfeng <evanfeng@pku.edu.cn>
2019-02-10 13:14:59 +0000

    add --vec, fix EXTRA_TOKENS

--- gen_vocab.py
+++ gen_vocab.py
@@ -1,34 +1,42 @@
 import argparse
 import os
 from utils.vocab import *
+from utils.data import *
 
 SRC_DIR = 'data/'
-LANGS = ['en', 'fr', 'de', 'jp']
 DOMAINS = ['books', 'dvd', 'music']
 PART = ['train.review', 'test.review', 'unlabeled.review']
 TRG_DIR = 'data/'
-EXTRA_TOKENS = ['<eos>', '<unk>']
+EXTRA_TOKENS = ['<eos>', '<unk>', '<num>']
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--size', type=int, default=10000, help='vocabulary size')
+    parser.add_argument('--vec', action='store_true', help='use fasttext')
+    parser.add_argument('--lang', choices=['en', 'fr', 'de', 'jp'], default=['en', 'fr', 'de', 'jp'], nargs='+', help='lanuages to gen vocab')
     parser.add_argument('--encoding', default='utf-8', help='encoding format')
     args = parser.parse_args()
 
     if not os.path.exists(TRG_DIR):
         os.makedirs(TRG_DIR)
 
-    for lang in LANGS:
-        corpus = []
-        for dom in DOMAINS:
-            for part in PART:
-                path = os.path.join(SRC_DIR, lang, dom, part)
-                with open(path, 'r', encoding=args.encoding) as fin:
-                    corpus += [row.rstrip() for row in fin]
+    for lang in args.lang:
+        if args.vec:
+            words, _ = load_vectors('data/wiki.{}.vec'.format('ja' if lang == 'jp'else lang), maxload=args.size)
+            vocab = Vocab()
+            for w in words:
+                vocab.add_word(w)
+        else:
+            corpus = []
+            for dom in DOMAINS:
+                for part in PART:
+                    path = os.path.join(SRC_DIR, lang, dom, part)
+                    with open(path, 'r', encoding=args.encoding) as fin:
+                        corpus += [row.rstrip() for row in fin]
+            vocab = Vocab(corpus)
+            vocab.cutoff(args.size)
 
-        vocab = Vocab(corpus)
-        vocab.cutoff(args.size)
         for tok in EXTRA_TOKENS:
             vocab.add_word(tok)
         vocab.save(os.path.join(TRG_DIR, 'vocab_{}.txt'.format(lang)))
