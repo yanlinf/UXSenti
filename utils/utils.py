e6c27514a05ff1810153a506b05deacdefddeb81
evanfeng <evanfeng@pku.edu.cn>
2019-02-23 11:41:30 +0000

    add sample

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,6 @@
+import torch
+
+
 def print_ids(ids, vocab):
     print(' '.join(vocab.idx2w[i] for i in ids))
 
@@ -17,3 +20,12 @@ def to_device(obj, cuda):
         return [to_device(x, cuda) for x in obj]
     else:
         return obj.cuda() if cuda else obj.cpu()
+
+
+def sample(x_list, n_samples, shuffle=True):
+    if shuffle:
+        size = x_list[0].size(0)
+        perm = torch.randperm(size)
+        return [x[perm][:n_samples] for x in x_list]
+    else:
+        return [x[:n_samples] for x in x_list]
