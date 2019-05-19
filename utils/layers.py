93bdec8d3e9b2120aa53dfbd6ff7cb62a0c2b3e1
evanfeng <evanfeng@pku.edu.cn>
2019-05-19 09:20:11 +0000

    Remove _weight_dropout WeightDropLSTM

--- utils/layers.py
+++ utils/layers.py
@@ -15,42 +15,6 @@ class GradReverse(torch.autograd.Function):
         return grad_output.neg()
 
 
-def _weight_drop(module, weights, dropout):
-    """
-    Helper for `WeightDrop`.
-    """
-
-    for name_w in weights:
-        w = getattr(module, name_w)
-        module.register_parameter(name_w + '_raw', nn.Parameter(w.data))
-        module._parameters[name_w] = F.dropout(w, p=dropout, training=False)
-
-    original_module_forward = module.forward
-
-    def forward(*args, **kwargs):
-        for name_w in weights:
-            raw_w = getattr(module, name_w + '_raw')
-            module._parameters[name_w] = F.dropout(raw_w, p=dropout, training=module.training)
-
-        return original_module_forward(*args)
-
-    setattr(module, 'forward', forward)
-
-
-class WeightDropLSTM(torch.nn.LSTM):
-    """
-    Wrapper around :class:`torch.nn.LSTM` that adds ``weight_dropout`` named argument.
-
-    Args:
-        weight_dropout (float): The probability a weight will be dropped.
-    """
-
-    def __init__(self, *args, weight_dropout=0.0, **kwargs):
-        super().__init__(*args, **kwargs)
-        weights = ['weight_hh_l' + str(i) for i in range(self.num_layers)]
-        _weight_drop(self, weights, weight_dropout)
-
-
 def dropout_mask(x, sz, p: float):
     """
     Return a dropout mask of the same type as `x`, size `sz`, with probability `p` to cancel an element.
